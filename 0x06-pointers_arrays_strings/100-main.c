4fe0ff957d6bf7dbcbd55c4c4aad0bfef9a1f2e8
root <root@Marieeah.localdomain>
2023-06-29 15:27:38 -0700

    100-main.c

---
+++ 0x06-pointers_arrays_strings/100-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated" ROT - 13) is a simple letter substitution cipher. \n";
+char *p;
--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,7 @@
 #include "main.h"
+
 #include <stdio.h>
+
 
 /**
 * rot13 - encoder rot13
@@ -9,10 +11,14 @@
 */
 
 char *rot13(char *s)
+
 {
+
 int i;
+
 int j;
-char data1[] = "ABBCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 for (i = 0; s[i] != '\0'; i++)
