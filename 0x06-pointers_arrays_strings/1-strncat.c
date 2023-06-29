499e8f0f42a8116ea5b4494283f5c50c5df63aa8
root <root@Marieeah.localdomain>
2023-06-29 13:34:57 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,11 @@
 #include "main.h"
-
 /**
 * _strncat - concatenate two strings
 * using at most n bytes from src
 * @dest: input value
 * @src: input value
 * @n: input value
+
 * Return: dest
 */
 char *_strncat(char *dest, char *src, int n)
@@ -21,7 +21,7 @@ i++;
 j = 0;
 while (j < n && src[j] != '\0')
 {
-dest[1] = src[j];
+dest[i] = src[j];
 i++;
 j++;
 }
