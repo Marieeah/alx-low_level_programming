ea8729ddd3694e40e398f9ad9e61c962c704411d
root <root@Marieeah.localdomain>
2023-06-29 09:05:52 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,7 +3,7 @@
 
 /**
 * rot13 - encoder rot13
-*@s: pointer to string params
+* @s: pointer to string params
 * Return: *s
 */
 
@@ -12,6 +12,8 @@ char *rot13(char *s)
 int i, j;
 char data1[] = "ABBCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+
 for (i = 0; s[i] != '\0'; i++)
 {
 for (j = 0; j < 52; j++)
