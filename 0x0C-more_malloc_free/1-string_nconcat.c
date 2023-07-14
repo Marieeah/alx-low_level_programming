4e3e9ed8423a1abcac078c18fde02d85203c3ce0
Marieeah <Omoleresoeso7@gmail.com>
2023-07-13 18:13:02 -0700

    Hot fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,11 +12,11 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
-unsigned int i, len1 = 0, len2 = 0;
+unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-while (s1 && *(s1 + len1))
+while (s1 && s1[len1])
 len1++;
-while (s2 && *(s2 + len2))
+while (s2 && s2[len2])
 len2++;
 
 if (n < len2)
@@ -27,16 +27,19 @@ s = malloc(sizeof(char) * (len1 + len2 + 1));
 if (!s)
 return (NULL);
 
-for (i = 0; i < len1; i++)
-*(s + i) = *(s1 + i);
+while (i < len1)
+{
+s[i] = s1[i];
+i++;
+}
 
-for (i = 0; n < len2 && i < (len1 + n); i++)
-*(s + i + len1) = *(s2 + i);
+while (n < len2 && i < (len1 + n))
+s[i++] = s2[j++];
 
-for (i = 0; n >= len2 && i < (len1 + len2); i++)
-*(s + i + len1) = *(s2 + i);
+while (n >= len2 && i < (len1 + len2))
+s[i++] = s2[j++];
 
-*(s + i + len1) = '\0';
+s[i] = '\0';
 
 return (s);
 }
