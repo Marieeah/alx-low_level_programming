bc0d12626c0a24a32c6f2761cf2141fc986ab47d
Marieeah <Omoleresoeso7@gmail.com>
2023-09-01 19:42:09 -0700

    Hotfix

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,10 +7,11 @@
 */
 int get_endianness(void)
 {
-	int num = 1;
-	char *endian = (char *)&num;
+	unsigned int x;
+	char *c;
 
-	if (*endian == 1)
-		return (1);
-return (0);
+	x = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
 }
